e0d3ca066dc5e423bca5163c37b9aaabeea9a00b
CookieMichal <mrcookiemichal@gmail.com>
2020-05-07 06:05:41 +0200

    Setup apollo client

--- webapp/src/modules/Root/Root.tsx
+++ webapp/src/modules/Root/Root.tsx
@@ -1,11 +1,13 @@
-import React, {FunctionComponent} from "react";
+import {ApolloProvider} from "@apollo/react-hooks";
 import App from "@modules/App";
+import React, {FunctionComponent} from "react";
+import apolloClient from "./apolloClient";
 
 const Root: FunctionComponent = () => {
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <App />
-    </>
+    </ApolloProvider>
   );
 };
 
